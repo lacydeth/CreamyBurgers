c47e812fcc4dab240acb14588ebf476e8927c0de
szabo.tamas <szabo.tamas@BURG-1E06-03>
2024-09-18 10:38:03 +0200

    login/register

--- CreamyBurgers/Login.xaml.cs
+++ CreamyBurgers/Login.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Windows;
+using EasyEncryption;
 
 namespace CreamyBurgers
 {
@@ -8,13 +12,57 @@ namespace CreamyBurgers
         {
             InitializeComponent();
         }
+        readonly private string conn = "server=localhost;port=3306;database=creamyburgers;user=root;password=";
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            LoadingBeetweenWindows loadingWindow = new LoadingBeetweenWindows();
-            loadingWindow.Show();
-            this.Close();
+            string username = tbUsername.Text.Trim();
+            string password = EasyEncryption.MD5.ComputeMD5Hash(tbPassword.Password);
+
+
+            if (username != "" && password != "")
+            {
+                try
+                {
+                    MySqlConnection sqlConn = new MySqlConnection(conn);
+                    using (sqlConn)
+                    {
+                        sqlConn.Open();
+                        string query = "SELECT username, password FROM users WHERE username=@username AND password=@password";
+
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(query, sqlConn);
+                        adapter.SelectCommand.Parameters.AddWithValue("@username", username);
+                        adapter.SelectCommand.Parameters.AddWithValue("@password", password);
+
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+
+                        if (table.Rows.Count > 0)
+                        {
+                            sqlConn.Close();
+                            LoadingBeetweenWindows loadingWindow = new LoadingBeetweenWindows();
+                            loadingWindow.Show();
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Hibás adatok!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+
+                    }
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Adj meg felhasználónevet és jelszót!", "Hiba", MessageBoxButton.OK,MessageBoxImage.Error);
+            }
         }
+
+            
 
         private void RegistrationButton_Click(object sender, RoutedEventArgs e)
         {
--- CreamyBurgers/Registration.xaml.cs
+++ CreamyBurgers/Registration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using MySql.Data.MySqlClient;
+using System.Windows;
 
 namespace CreamyBurgers
 {
@@ -9,6 +10,8 @@ namespace CreamyBurgers
             InitializeComponent();
         }
 
+        readonly private string conn = "server=localhost;port=3306;database=creamyburgers;user=root;password=";
+        
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Regisztrációs sikeres!");
@@ -19,9 +22,39 @@ namespace CreamyBurgers
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            this.Close();
+            try
+            {
+                MySqlConnection sqlConn = new MySqlConnection(conn);
+                using (sqlConn)
+                {
+                    sqlConn.Open();
+                    string query = "INSERT into users (username, password, email, permID, CreationDate) VALUES (@username, @password, @email, @permID, @creationdate)";
+
+                    MySqlCommand sqlCommand = new MySqlCommand(query, sqlConn);
+                    sqlCommand.Parameters.AddWithValue("@username", tbUsername.Text.ToLower());
+                    sqlCommand.Parameters.AddWithValue("@password", EasyEncryption.MD5.ComputeMD5Hash(tbPassword.Password));
+                    sqlCommand.Parameters.AddWithValue("@email", tbEmail.Text);
+                    sqlCommand.Parameters.AddWithValue("@permID", 1);
+                    sqlCommand.Parameters.AddWithValue("@creationdate", DateTime.Now);
+
+                    int status = sqlCommand.ExecuteNonQuery();
+
+                    if (status > 0)
+                    {
+                        MessageBox.Show("Sikeres Regisztráció");
+                        sqlConn.Close();
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        this.Close();
+                    }
+
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+
         }
     }
 }
